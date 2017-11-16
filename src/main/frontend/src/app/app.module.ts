f33133f4b98ba7bca128fc1bf4d0f91e34853c2c
faltdor <faltdor@gmail.com>
2017-11-16 17:46:29 -0500

    Add all components for task module

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksAddComponent } from './components/tasks/tasks-add/tasks-add.component';
+import { TasksListComponent } from './components/tasks/tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TasksAddComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule
